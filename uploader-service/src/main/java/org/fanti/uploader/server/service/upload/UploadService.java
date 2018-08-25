5ca1b052982c677787ddeacfa01f90658b265392
范天凯 <ftk@servyou.com.cn>
2018-08-25 17:20:33 +0800

    将上传逻辑放到Service中

---
+++ uploader-service/src/main/java/org/fanti/uploader/server/service/upload/UploadService.java
@@ -0,0 +1,9 @@
+package org.fanti.uploader.server.service.upload;
+
+import org.apache.commons.fileupload.FileItem;
+
+import java.util.List;
+
+public interface UploadService {
+    void upload(List<FileItem> list);
+}
